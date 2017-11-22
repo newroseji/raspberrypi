360ff8515355455e968b498b47507c5eef553cc2
Niraj <nirajbjk@gmail.com>
2017-11-22 15:23:32 -0500

    GPIO pins set for manual movements.

--- manual.py
+++ manual.py
@@ -3,90 +3,84 @@ import time
 import sys
 import Tkinter as tk
 
-def init():
-        
-        gpio.setmode(gpio.BOARD)
-	gpio.setup(7, gpio.OUT)
-    	gpio.setup(11, gpio.OUT)
-    	gpio.setup(13, gpio.OUT)
-    	gpio.setup(15, gpio.OUT)
 
-	print 'Initialization done!'
-    
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+    print 'Initialization done!'
+
+
 def forward(tf):
-        
-       
     print 'Moving Forward...'
-    gpio.output(11, False)
-    gpio.output(13, False)
-    gpio.output(7, True)  #New Line
-    gpio.output(15, True) #New Line
-    
+    gpio.output(7, True)
+    gpio.output(13, True)
+    gpio.output(11, False)  # New Line
+    gpio.output(15, False)  # New Line
+
     time.sleep(tf)
     gpio.cleanup()
+
 
 def reverse(tf):
-   
     print 'Moving Backward...'
-    gpio.output(7, False)
-    gpio.output(15, False)
-    gpio.output(11, True) #New Line
-    gpio.output(13, True) #New Line
-    
+    gpio.output(11, True)
+    gpio.output(15, True)
+    gpio.output(7, False)  # New Line
+    gpio.output(13, False)  # New Line
+
     time.sleep(tf)
-    gpio.cleanup()    
+    gpio.cleanup()
+
 
 def turn_left(tf):
-
-   
     print 'Turning Left...'
-    gpio.output(11, False)
-    gpio.output(7, False)  #New Line
-    gpio.output(13, True) #New Line
-    gpio.output(15, False) #New Line
+    gpio.output(7, True)
+    gpio.output(15, False)  # New Line
+    gpio.output(13, False)  # New Line
+    gpio.output(11, False)  # New Line
     time.sleep(tf)
     gpio.cleanup()
+
 
 def turn_right(tf):
-
- 
     print 'Turning Right...'
-    gpio.output(13, False)
-    gpio.output(7, False)  #New Line
-    gpio.output(11, True) #New Line
-    gpio.output(15, False) #New Line
-    
-    time.sleep(tf)
-    gpio.cleanup()    
+    gpio.output(11, True)
+    gpio.output(13, False)  # New Line
+    gpio.output(7, False)  # New Line
+    gpio.output(15, False)  # New Line
 
-def pivot_left(tf):
-
-
-   
-    print 'Pivoting Left...'
-    gpio.output(11, False)
-    gpio.output(15, False)
-    gpio.output(7, True) #New Line
-    gpio.output(13, True) #New Line
-    time.sleep(tf)
-    gpio.cleanup()    
-
-def pivot_right(tf):
-
-
- 
-    print 'Pivoting Right...'
-    gpio.output(7, False)
-    gpio.output(13, False)
-    gpio.output(11, True) #New Line
-    gpio.output(15, True) #New Line
     time.sleep(tf)
     gpio.cleanup()
+
+
+def pivot_left(tf):
+    print 'Pivoting Left...'
+    gpio.output(7, True)
+    gpio.output(15, True)
+    gpio.output(13, False)  # New Line
+    gpio.output(11, False)  # New Line
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+def pivot_right(tf):
+    print 'Pivoting Right...'
+    gpio.output(13, True)
+    gpio.output(11, True)
+    gpio.output(7, False)  # New Line
+    gpio.output(15, False)  # New Line
+    time.sleep(tf)
+    gpio.cleanup()
+
 
 def key_input(event):
     init()
 
-    print 'Key: ' , event.char
+    print 'Key: ', event.char
     key_press = event.char
     sleep_time = 0.030
 
@@ -104,7 +98,8 @@ def key_input(event):
         pivot_right(sleep_time)
     else:
         pass
-        
+
+
 command = tk.Tk()
-command.bind('<KeyPress>',key_input)
-command.mainloop()    
+command.bind('<KeyPress>', key_input)
+command.mainloop()
